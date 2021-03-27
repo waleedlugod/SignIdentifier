063c8f7c79a473eb7dcc0a6417a19626d8dd19c7
wallrusi <lugodwaleed@gmail.com>
2021-03-27 22:26:34 +0800

    Add project files.

---
+++ SignIdentifier/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SignIdentifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double operand;
+            int negativeCount = 0;
+
+            for (int i = 1; i < 3; i++)
+            {
+                string ordinal = "th";
+                if (i == 3)
+                {
+                    ordinal = "rd";
+                }
+                else if (i == 2)
+                {
+                    ordinal = "nd";
+                }
+                else if (i == 1)
+                {
+                    ordinal = "st";
+                }
+
+                Console.WriteLine("Enter " + i + ordinal + " value: ");
+                operand = double.Parse(Console.ReadLine());
+
+                if (operand < 0)
+                {
+                    negativeCount++;
+                }
+            }
+
+            Console.WriteLine("The sign of the answer is {0}", (negativeCount % 2 == 0) ? "positive." : "negative.");
+        }
+    }
+}
